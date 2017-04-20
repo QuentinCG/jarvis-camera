295c6da788ac25a40c98f0d9b6f63ea9b0fac2ff
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-04-20 20:59:38 +0200

    plugin: Fix issue (minor)

--- functions.sh
+++ functions.sh
@@ -9,7 +9,7 @@ jv_pg_ca_take_picture()
   avconv -f video4linux2 -s $var_jv_pg_ca_camera_resolution -i $var_jv_pg_ca_camera_input_filename -ss 0:0:$var_jv_pg_ca_stream_before_taking_picture -frames 1 $1
 
   # Finish here if need to say nothing
-  if [[ ! $2 =~ "True" ]]; then
+  if [[ $2 =~ "True" ]]; then
     return
   fi
 
