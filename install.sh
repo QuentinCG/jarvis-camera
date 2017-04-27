c921cbf3f46b1bca472c5f7a8746a0ffdeed801a
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-04-27 18:10:45 +0200

    install: Force to install by default

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Install avconv (in libav-tools package)
-[[ -z $(which libav-tools) ]] && sudo apt-get install libav-tools
+[[ -z $(which libav-tools) ]] && sudo apt-get --yes --force-yes install libav-tools
